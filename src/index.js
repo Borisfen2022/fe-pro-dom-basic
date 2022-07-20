99a6a45237582a413d4242da3522cf950a2a235c
Olya_Koshelnikova <olha.koshelnikova@gmail.com>
2022-07-21 01:23:00 +0300

    basic DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const elements = document.querySelectorAll('li.item');
 
-export const findElement = () => {};
+  elements.forEach((element, index) => {
+    if (index % 2 === 0) {
+      element.style.backgroundColor = 'red';
+    }
+  });
+};
+
+export const findElement = () => {
+  let element = document.querySelector('li.item').nextElementSibling;
+
+  while (element) {
+    if (element.classList.contains('likedItem')) {
+      element.style.backgroundColor = 'blue';
+      break;
+    }
+
+    element = element.nextElementSibling;
+  }
+};
